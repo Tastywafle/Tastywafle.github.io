23822e2cac366baea43b256f708f70fd7f3bc25e
Tastywafle <143985204+Tastywafle@users.noreply.github.com>
2023-12-26 08:51:21 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const forms= document.querySelector(.forms), 
+      pwShowHide = document.querySelectorAll(.eye-icon),
+      links = document.querySelectorAll(.link);
 
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password")
+
+        pwFields.forEach(password => {
+            if(password.type === ""){
+              password.type = "text";
+              eyeIcon.classList.replace("bx-hide")
+            }
+        })
+      
+    })
+})
