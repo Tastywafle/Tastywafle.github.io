39c12c8ebc45f314eddd3741b37c782e4c9d684c
Tastywafle <143985204+Tastywafle@users.noreply.github.com>
2023-12-30 20:07:39 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const forms= document.querySelector(".forms"),
 
 pwShowHide.forEach(eyeIcon => {
     eyeIcon.addEventListener("click", () => {
-        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password")
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
 
         pwFields.forEach(password => {
-            if(password.type === ""){
+            if(password.type === "password"){
                 password.type = "text";
                 eyeIcon.classList.replace("bx-hide", "bx-show");
                 return;
