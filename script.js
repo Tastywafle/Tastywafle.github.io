d96bfffc029bbb5208702bb954fe7dc5662e57d4
Tastywafle <143985204+Tastywafle@users.noreply.github.com>
2024-01-06 12:29:52 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-analytics.js";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+apiKey: "AIzaSyChRrxkNn8QvJOb29Q3fraIj_kiS7-i4QI",
+authDomain: "whiteboard-data-9bfc2.firebaseapp.com",
+projectId: "whiteboard-data-9bfc2",
+storageBucket: "whiteboard-data-9bfc2.appspot.com",
+messagingSenderId: "957839364618",
+appId: "1:957839364618:web:39e606c73e1c836bfa05f3",
+measurementId: "G-ZXGLN916W3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+//get ref to database services
+const db = getDatabase(app);
+
+document.getElementById
+
 const forms= document.querySelector(".forms"), 
       pwShowHide = document.querySelectorAll(".eye-icon"),
       links = document.querySelectorAll(".link");
